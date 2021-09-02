04c213718d7374083836b045b408c7aad6b8254c
Eric Mariot <ericmariots@gmail.com>
2021-09-02 19:15:23 -0300

    Everyone can CRUD!

--- config/settings.py
+++ config/settings.py
@@ -38,9 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # 3rd-party app
+    'rest_framework',
+    
     # Local
-    'posts',
+    'posts.apps.PostsConfig',
 ]
+
+REST_FRAMEWORK = {
+  'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.AllowAny',
+  ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
