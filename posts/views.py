0422405790caec32b9ce16cc3738383e77d2d772
Eric Mariot <ericmariots@gmail.com>
2021-09-02 19:36:15 -0300

    Update api permissions - authorization

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 # Creates 2 views
 class PostList(generics.ListCreateAPIView):
@@ -8,5 +9,6 @@ class PostList(generics.ListCreateAPIView):
   serializer_class = PostSerializer
   
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = (IsAuthorOrReadOnly,)
   queryset = Post.objects.all()
   serializer_class = PostSerializer
